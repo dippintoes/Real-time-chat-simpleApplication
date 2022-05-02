aea4f0355a81f012f058ccaa4b3e92a85dc86459
Rutuja Gosavi <gosavirutu523@gmail.com>
2022-05-02 16:17:54 +0530

    done

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const io = require('socket.io')(4000)
 
 const users = {}
+
 io.on('connection',socket=>{
     console.log('new user')
     socket.emit('chat-message','Hello People')
-    socket.on('new-user',name=>{
+
+    socket.on('new-user',name=>
+    {
         users[socket.id]=name
         socket.broadcast.emit('user-connected',name)
     })
-    socket.on('send-chat-message',message=>{
+
+    socket.on('send-chat-message',message=>
+    {
         //console.log(message); to check if message is sending
-        socket.broadcast.emit('chat-message',{message:message,name:users[socket.id]})//to send to all users expect himself
+        socket.broadcast.emit('chat-message',
+        {
+            message:message,
+            name:users[socket.id]
+        })//to send to all users expect himself
     })
-    socket.on('disconnect',()=>{
-        
+    socket.on('disconnect',()=>
+    {
         socket.broadcast.emit('user-disconnected',users[socket.id])
         delete users[socket.id]
     })
